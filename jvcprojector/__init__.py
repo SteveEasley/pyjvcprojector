84ed1942ad8b3b66bc81f8dc77e920c32a9f5b26
iloveicedgreentea <31193909+iloveicedgreentea@users.noreply.github.com>
2024-11-24 14:16:13 -0500

    fix imports

--- jvcprojector/__init__.py
+++ jvcprojector/__init__.py
@@ -2,4 +2,12 @@
 
 # ruff: noqa: F401
 
+from .error import (
+    JvcProjectorAuthError,
+    JvcProjectorCommandError,
+    JvcProjectorConnectError,
+    JvcProjectorError,
+)
+from .projector import JvcProjector
+
 __version__ = "1.1.2"
